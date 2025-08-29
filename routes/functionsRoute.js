1a37fe3ef03e79a1da37b297c71560d932300f2c
officialaditshrm <officialaditshrm@gmail.com>
2025-08-29 11:31:45 +0530

    GET message

--- routes/functionsRoute.js
+++ routes/functionsRoute.js
@@ -5,6 +5,14 @@ import functionArray from '../controllers/functionsController.js'
 
 const router = express.Router()
 
+router.get("/", (req, res) => {
+  res.json({
+    message: "Please use 'POST' method on this URI and post your request in this format:",
+    example: { data: ["2", "a", "y", "4", "&", "-", "*", "5", "92", "b"] }
+  });
+});
+
+
 router.post("/", functionArray)
 
 export default router
